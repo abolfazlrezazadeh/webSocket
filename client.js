67bc0e0def2e0fd161ae44797612c524d51e6e2c
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-09-15 13:11:04 +0330

    change buffer type to string and show backend messages in frontend

--- client.js
+++ client.js
@@ -1,4 +1,8 @@
 const socket = new WebSocket("ws://localhost:3001");
-socket.onopen = (event=>{
-    socket.send('hello something')
-})
+socket.onopen = (event) => {
+  socket.send("hello something from client");
+};
+socket.onmessage = (event) => {
+  socket.send("i want to read ur data");
+  document.write(event.data);
+};
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const http = require("http")
-const WebSocket = require("ws")
+const { log } = require("console");
+const http = require("http");
+const WebSocket = require("ws");
 const server = http.createServer();
-const ws = new WebSocket.Server({server})
-ws.on("headers",(headers, req)=>{
-    console.log(headers);
-})
+const ws = new WebSocket.Server({ server });
+ws.on("headers", (headers, req) => {
+  console.log(headers);
+});
+ws.on("connection", (socket, req) => {
+    socket.on('message',(data)=>{
+        console.log(data.toString());
+    })
+  socket.send("hello! client, from server 'file'");
+});
 
-server.listen("3001",()=>{console.log("running on port 3001 ...");})
+server.listen("3001", () => {
+  console.log("running on port 3001 ...");
+});
