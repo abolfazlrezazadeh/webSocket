ae9acb9927485472f28e202623f52f0bf2565b25
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-09-18 12:00:31 +0330

    save message in localstorage

--- client.js
+++ client.js
@@ -1,16 +1,38 @@
-const socket = io("http://localhost:3001", {
-  query: {
-    value1: "hello",
-    value2: "goodby",
-  },
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        authorization: "bearer <token>",
-      },
-    },
-  },
-});
+function intializeChatBox() {
+  let localStorageValue = localStorage.getItem("messages");
+  const messages = (localStorageValue ? localStorageValue.split("#") : []).map(
+    (item) => {
+      if (item) return item;
+    }
+  );
+  messages.forEach((item) => {
+    const paragraphElement = document.createElement("p");
+    paragraphElement.innerText = item;
+    const chatBox = document.querySelector(".chatbox");
+    chatBox.appendChild(paragraphElement);
+  });
+}
+intializeChatBox();
+const socket = io("http://localhost:3001");
 socket.on("connect", (data) => {
-  console.log(data);
+  const sendBtn = document.getElementById("sendBtn");
+  sendBtn.addEventListener("click", (e) => {
+    const textBox = document.getElementById("text");
+    const message = textBox.value;
+    if (!message) {
+      return alert("you dont allowed to send empty message");
+    }
+    socket.emit("clientMessage", message);
+    textBox.value;
+  });
+});
+socket.on("serverMessage", (message) => {
+  let localStorageValue = localStorage.getItem("messages")
+    ? localStorage.getItem("messages") + "#" + message
+    : message;
+  localStorage.setItem("messages", localStorageValue);
+  const paragraphElement = document.createElement("p");
+  paragraphElement.innerText = message;
+  const chatBox = document.querySelector(".chatbox");
+  chatBox.appendChild(paragraphElement);
 });
--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const io = socketIO(server, {
 });
 // const ws = new WebSocket.Server({ server });
 io.on("connection", (socket) => {
-   console.log(socket.handshake.query);
-   console.log(socket.handshake.headers['authorization']);
+   socket.on('clientMessage' ,data =>{
+    io.emit('serverMessage' , data)
+   })
 });
 server.listen("3001", () => {
   console.log("running on port 3001 ...");
